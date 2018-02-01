179cca6da761aba69d020e7112724c5420b73f70
Ataeraxia <you@example.com>
2018-02-01 16:34:15 -0500

    Well, this is a problem

--- imports/ui/Blog.js
+++ imports/ui/Blog.js
@@ -3,7 +3,17 @@ import React, { Component } from "react";
 export default class Blog extends Component {
     render() {
         return (
-            <li className="tile">{this.props.blog.title}</li>
+            <li className="tile">
+                <div>
+                    <h2>
+                        {this.props.blog.title}
+                    </h2>
+
+                    <p>
+                        {this.props.blog.body}
+                    </p>
+                </div>
+            </li>
         );
     }
 }
