342e66a0c5dcd3302c89bb1d591c909142f6656b
Ataeraxia <you@example.com>
2018-02-01 10:52:41 -0500

    Init

---
+++ imports/ui/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+import Blog from "./Blog.js";
+
+export default class App extends Component {
+    getBlogs() {
+        return [
+            { _id: 0, title: 'This is the 0th blog post'},
+            { _id: 1, title: 'This is the 1st blog post'},
+            { _id: 2, title: 'This is the 2nd blog post'}
+        ];
+    }
+
+    renderBlogs() {
+        return this.getBlogs().map((blog) => {
+            <Blog key={blog._id} blog={blog} />
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>literify</h1>
+                </header>
+
+                <ul>
+                    {this.renderBlogs()}
+                </ul>
+            </div>
+        );
+    }
+}
