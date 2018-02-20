54eb8a7a2ad1368e67509122c43914b0dc350ea0
Ataeraxia <you@example.com>
2018-02-20 18:04:16 -0500

    Attempt to hook up BrandNav.

--- client/main.js
+++ client/main.js
@@ -4,6 +4,8 @@ import { render } from "react-dom";
 import { FlowRouter } from "meteor/kadira:flow-router";
 
 import App from "../imports/ui/App.js";
+import BrandNav from "../imports/ui/containers/BrandNav.js";
 
 Meteor.startup(() => {
+  render(<BrandNav />, document.getElementById('render-target'));
 });
