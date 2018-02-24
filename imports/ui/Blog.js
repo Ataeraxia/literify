7e36a930dc5238f22f67722d224f0dca27caec1c
Ataeraxia <you@example.com>
2018-02-24 18:25:13 -0500

    Add ability to delete blogs. Style delete button.

--- imports/ui/Blog.js
+++ imports/ui/Blog.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 
+import { Blogs } from "../api/blogs.js";
+
 export default class Blog extends Component {
+    punctuation(text) {
+        if(text && text.length > 250){
+            return "...";
+        } else {
+            return "";
+        }
+    }
+
+    deleteBlog() {
+        Blogs.remove(this.props.blog._id);
+    }
+
     render() {
         return (
             <li className="tile">
@@ -10,8 +24,12 @@ export default class Blog extends Component {
                     </h2>
 
                     <p>
-                        {this.props.blog.body.substring(0,250)}...
+                        {this.props.blog.body.substring(0,250)}{this.punctuation(this.props.blog.body)}
                     </p>
+
+                    <button onClick={this.deleteBlog.bind(this)}>
+                        &times;
+                    </button>
                 </div>
             </li>
         );
