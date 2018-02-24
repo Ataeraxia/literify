c544805798d8bbb3a5c38ff5662d041668555d2d
Ataeraxia <you@example.com>
2018-02-24 17:14:19 -0500

    Add database. Add form for creating new blogs. Forget to use git.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
+import { withTracker } from "meteor/react-meteor-data";
+
+import { Blogs } from "../api/blogs.js";
 
 import Blog from "./Blog.js";
-import { allstar } from "./allstar.js";
+import AddBlogForm from "./components/AddBlogForm.js";
 
-export default class App extends Component {
-    getBlogs() {
-        return [
-            {
-                _id: 0, title: 'This is the 0th blog post', body: allstar},
-            { _id: 1, title: 'This is the 1st blog post', body: allstar},
-            { _id: 2, title: 'This is the 2nd blog post', body: allstar}
-        ];
-    }
-
+class App extends Component {
     renderBlogs() {
-        return this.getBlogs().map((blog) => (
+        return this.props.blogs.map((blog) => (
             <Blog key={blog._id} blog={blog} />
-        ));
+        ))
     }
 
     render() {
@@ -26,6 +20,8 @@ export default class App extends Component {
                     <h1>literify</h1>
                 </header>
 
+                <AddBlogForm />
+
                 <ul className="sidewalk">
                     {this.renderBlogs()}
                 </ul>
@@ -33,3 +29,9 @@ export default class App extends Component {
         );
     }
 }
+
+export default withTracker(() => {
+    return {
+        blogs: Blogs.find({}, { sort: { createdAt: -1 }}).fetch(),
+    };
+})(App);
---
+++ imports/ui/components/AddBlogForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import { Blogs } from "../../api/blogs.js";
+
+export default class AddBlogForm extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const title = ReactDOM.findDOMNode(this.refs.titleTextInput).value.trim();
+
+        const body = ReactDOM.findDOMNode(this.refs.bodyTextArea).value.trim();
+
+        Blogs.insert({
+            title,
+            body,
+            createdAt: new Date(),
+        });
+
+        ReactDOM.findDOMNode(this.refs.titleTextInput).value = '';
+
+        ReactDOM.findDOMNode(this.refs.bodyTextArea).value = '';
+    }
+    render() {
+        return (
+            <div className="form-container">
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <input
+                        type="text"
+                        ref="titleTextInput"
+                        placeholder="Title"
+                    />
+                    <textarea ref="bodyTextArea"/>
+                    <input
+                        type="submit"
+                        value="Post"
+                    />
+                </form>
+            </div>
+        );
+    }
+}
