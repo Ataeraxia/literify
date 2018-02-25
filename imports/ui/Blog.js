401f58d27e039e10283b80bedcae34078f646523
Ataeraxia <you@example.com>
2018-02-25 13:46:08 -0500

    Hook up show ability in Blog. Is really cool.

--- imports/ui/Blog.js
+++ imports/ui/Blog.js
@@ -3,11 +3,38 @@ import React, { Component } from "react";
 import { Blogs } from "../api/blogs.js";
 
 export default class Blog extends Component {
+    constructor(){
+        super();
+        this.setFullBlog = this.setFullBlog.bind(this);
+        this.renderBlogBody = this.renderBlogBody.bind(this);
+        this.state = {isFull: false};
+    }
+
     punctuation(text) {
         if(text && text.length > 250){
             return "...";
         } else {
             return "";
+        }
+    }
+
+    renderBlogBody(){
+        if(this.state.isFull){
+            return this.props.blog.body;
+        } else {
+            return "" + this.props.blog.body.substring(0, 250) + this.punctuation(this.props.blog.body); 
+        }
+    }
+
+    setFullBlog() {
+        if(this.state.isFull == true) {
+            this.setState({
+                isFull: false
+            });
+        } else {
+            this.setState({
+                isFull: true
+            });
         }
     }
 
@@ -17,18 +44,20 @@ export default class Blog extends Component {
 
     render() {
         return (
-            <li className="tile">
-                <div>
-                    <h2>
-                        {this.props.blog.title}
-                    </h2>
+            <li className="tile" onClick={this.setFullBlog}>
+                <div >
+                    <div className="blog-head">
+                        <h2>
+                            {this.props.blog.title}
+                        </h2>
 
-                    <h4>
-                        {this.props.blog.createdAt.toDateString()}
-                    </h4>
+                        <h4>
+                            {this.props.blog.createdAt.toDateString()}
+                        </h4>
+                    </div>
 
                     <p>
-                        {this.props.blog.body.substring(0,250)}{this.punctuation(this.props.blog.body)}
+                        {this.renderBlogBody()}
                     </p>
 
                     <button onClick={this.deleteBlog.bind(this)}>
